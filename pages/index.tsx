e081818752034fffb9ea7a3b06ef25402e29c4c4
파링 <parangee9706@gmail.com>
2022-01-18 09:49:55 +0900

    backup before installing twemoji

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 const Home: NextPage = () => {
-  return <div className="mt-8 container mx-auto">asdf</div>
+  return (
+    <div className="mt-8 container mx-auto">
+      <div className="text-3xl font-bold">About me</div>
+      <div className="text-xl">pikokr / 14y / Student web developer</div>
+    </div>
+  )
 }
 
 export default Home

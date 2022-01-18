62823d8200566388d181f6c11b2aa7929c7895c0
파링 <parangee9706@gmail.com>
2022-01-18 10:17:23 +0900

    first section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import type { NextPage } from 'next'
+import twemoji from 'twemoji'
 
 const Home: NextPage = () => {
   return (
-    <div className="mt-8 container mx-auto">
+    <div className="mt-8 container mx-auto gap-2 flex flex-col">
       <div className="text-3xl font-bold">About me</div>
       <div className="text-xl">pikokr / 14y / Student web developer</div>
+      <div className="flex gap-2 items-center text-xl">
+        <span
+          dangerouslySetInnerHTML={{
+            __html: twemoji.parse('🏳️‍⚧️', { className: 'w-8 h-8', folder: 'svg', ext: '.svg' }),
+          }}
+        />
+        she/her
+      </div>
+      <div className="flex select-none gap-2">
+        <a href="https://github.com/pikokr" rel="noreferrer" target="_blank" className="flex text-xl gap-2">
+          <FontAwesomeIcon className="text-2xl" icon={['fab', 'github']} />
+          GitHub
+        </a>
+        <a href="https://discord.gg/EkFHRG4TZZ" rel="noreferrer" target="_blank" className="flex text-xl gap-2">
+          <FontAwesomeIcon className="text-2xl" icon={['fab', 'discord']} />
+          Discord
+        </a>
+        <a href="https://twitter.com/pikokr_" rel="noreferrer" target="_blank" className="flex text-xl gap-2">
+          <FontAwesomeIcon className="text-2xl" icon={['fab', 'twitter']} />
+          Twitter
+        </a>
+        <a href="https://youtube.com/c/pikokr" rel="noreferrer" target="_blank" className="flex text-xl gap-2">
+          <FontAwesomeIcon className="text-2xl" icon={['fab', 'youtube']} />
+          YouTube
+        </a>
+      </div>
+      <div className="text-xl">
+        Working in{' '}
+        <a href="https://github.com/kimusoft" className="border-b" rel="noreferrer" target="_blank">
+          KimuSoft
+        </a>
+      </div>
     </div>
   )
 }
